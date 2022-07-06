ef3c5d6d7b0d63266bb7a70adbe877eaf09e5eb9
Kolya Dubyna <mykola22dubyna@gmail.com>
2022-07-06 14:38:01 +0300

    New local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -126,3 +126,8 @@ STATIC_ROOT = BASE_DIR / 'static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+try:
+    from .local_settings import*
+except ImportError:
+    print("looks like no local settings. You must be on production")
